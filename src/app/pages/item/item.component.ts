8708e63787b34bb26620aaa65b5977df27a75e7c
IvanBecerraA <ivanbecerra91@gmail.com>
2023-03-01 10:33:27 -0300

    Fin sección 7

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductosService } from '../../services/productos.service';
+import { ProductoDescripcion } from '../../interfaces/producto-descripcion.interface';
 
 @Component({
   selector: 'app-item',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent {
+
+  producto: ProductoDescripcion = {};
+  id: any;
+
+
+  constructor( private route: ActivatedRoute,
+               public productoService: ProductosService ) {}
+
+  ngOnInit(): void {
+
+    this.route.params
+    .subscribe( parametros => {
+      console.log(parametros['id']);
+
+      this.productoService.getProducto(parametros['id'])
+        .subscribe( (producto: any) => {
+          this.id = parametros['id']
+          this.producto = producto;
+        });
+    });
+    
+  }
 
 }
